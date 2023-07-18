65e63094651a6b7adb034b431ce1ee3230453190
minhdq0709 <doquangminh0709@gmail.com>
2023-07-18 14:15:44 +0700

    edit handle db

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ module.exports = {
     'connection': {
         'host': '192.168.23.22',
         'port': '3306',
-        'user': 'minhdq', // social_index_v2
-        'password': 'wgy2FdMt0rXfcmCWGSqa',//    kSPQ6INMge4p3VFn1bCX
+        'user': 'social_index_v2', // social_index_v2
+        'password': 'kSPQ6INMge4p3VFn1bCX',// wgy2FdMt0rXfcmCWGSqa   
         'multipleStatements': true
     }
 };
